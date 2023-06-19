5e8f7d773125bedbe8b32eb2b598ad72c4c78107
K <joleocl@gmail.com>
2023-06-19 17:11:20 -0300

    feat: github action

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 import test from 'node:test';
 import assert from 'node:assert';
-import contrib from './contrib.json' assert { type: "json" }
-import Schema from './src/model.mjs';
+import contrib from '../contrib.json' assert { type: "json" }
+import Schema from '../src/model.mjs';
 
 test("check contrib", () => {
     const result = Schema.safeParse(contrib);
