066db4e7872388ec9b9d116b93fa65b464ecc9be
K <joleocl@gmail.com>
2023-06-22 13:11:13 -0300

    feat: add tests

--- src/model.mjs
+++ src/model.mjs
@@ -1,12 +1,26 @@
 import z from 'zod';
 
-const Schema = z.array(
-  z.object({
-    id: z.string(),
-    title: z.string(),
-    signal: z.string(),
-    url: z.string().url(),
-  })
-)
+const maxChars = 40;
+const minChars = 2;
+
+const Schema = z.object({
+  id: z.string()
+    .min(minChars)
+    .max(maxChars),
+  title: z.string()
+    .min(minChars)
+    .max(maxChars),
+  signal: z.string()
+    .min(minChars)
+    .max(maxChars),
+  url: z.string().url(),
+  tags: z.optional(
+    z.array(z.string()
+      .min(minChars)
+      .max(maxChars)
+    )
+  )
+}).strict();
+
 
 export default Schema;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,57 @@
 import test from 'node:test';
 import assert from 'node:assert';
-import contrib from '../contrib.json' assert { type: "json" }
+import fs from 'node:fs';
+import split from 'split2';
 import Schema from '../src/model.mjs';
+import _ from 'lodash';
 
-test("check contrib", () => {
-    const result = Schema.safeParse(contrib);
-    assert.equal(result.success, true, result.error);
+const contribFile = './contrib.json'
+test("check contrib", async () => {
+    return new Promise((resolve) => {
+        fs.createReadStream(contribFile)
+            .pipe(split())
+            .on('data', validate)
+            .on('end', resolve)
+    });
+});
+
+test("check contrib check unique ids", async () => {
+    unique('id')
 })
+
+test("check contrib check unique urls", async () => {
+    unique('url')
+})
+
+function unique(path){
+    const data = fs.readFileSync(contribFile)
+        .toString()
+        .split('\n')
+        .filter(d => d !== '')
+        .map(JSON.parse);
+    const repeated = _.chain(data)
+        .map(r => r[path])
+        .groupBy((k) => k)
+        .mapValues()
+        .filter(g => g.length > 1)
+        .map(r => _.head(r))
+        .uniq()
+        .value();
+    assert.equal(0, repeated.length, `repeated identifiers: ${JSON.stringify(repeated)}`)
+}
+
+function validate(data) {
+    const json = parseJSON(data);
+    assert.ok(json, `invalid json check it ${data}`)
+    const result = Schema.safeParse(json);
+    assert.equal(result.success, true, `check data ${data} \n\n` + result.error);
+}
+
+function parseJSON(data) {
+    try {
+        return JSON.parse(data)
+    } catch (e) {
+        return null
+    }
+}
+
